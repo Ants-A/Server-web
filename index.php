14efab861724676ec40d60b0a078622a3ac110b7
Ants Aas <ants.aas.v65@gmail.com>
2026-09-10 09:04:55 +0300

    Function examples

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
-$test = 10; //AAAAAAAAAAAAAAAAAAAAA
-$test2 = ++$test;
-var_dump($test, $test2);
-?>
+ 
+function helloName($name='Nameless', $age='unknown') 
+{
+  var_dump("Hello , $name! You are $age old!");
+}
+
+helloName('Kaspar', 33);
+helloName('Mats', 69);
+helloName('Ants', 18);
+
+function square($a) 
+{
+  if ($a<0)
+  {
+    return 0;
+  }
+  return $a * $a;
+}
+
+$answer = square(2);
+
+function recursion($i) {
+  if ($i < 10)
+  {
+    var_dump($i);
+    recursion($i+1);
+  }
+}
+
+recursion(0);
